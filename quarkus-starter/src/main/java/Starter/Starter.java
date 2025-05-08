d7f58fa97001ff44c57fc37a71fb9ce235fe68f9
Matteo Di Carlo <mat.dicarlo@gmail.com>
2025-05-08 16:07:18 +0200

    quarkus starter project

---
+++ quarkus-starter/src/main/java/Starter/Starter.java
@@ -0,0 +1,13 @@
+package Starter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Starter {
+    private static final Logger log = LoggerFactory.getLogger(Starter.class);
+
+    public void main() throws Exception {
+        log.info("Starter");
+
+    }
+}
